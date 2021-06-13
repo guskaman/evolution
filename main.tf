87a4f43d646e5a1cd693b0d8b1cdcf512daec5cf
guseman <guseman@gmail.com>
2021-06-13 21:00:23 +0300

    access key removed

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-1"
-  access_key = "$AWS_ACCESS_KEY_ID"
-  secret_key = "$AWS_SECRET_ACCESS_KEY"
+  # access_key = "$AWS_ACCESS_KEY_ID"
+  # secret_key = "$AWS_SECRET_ACCESS_KEY"
 }
 
 resource "aws_vpc" "main" {
