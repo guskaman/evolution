be40937014b80b11a5f5936e55e216f02561d75e
guseman <guseman@gmail.com>
2021-06-13 22:30:16 +0300

    role added

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "eu-west-1"
-  # access_key = "$AWS_ACCESS_KEY_ID"
-  # secret_key = "$AWS_SECRET_ACCESS_KEY"
 }
 
 terraform {
@@ -12,6 +10,8 @@ terraform {
   }
 }
 
+################# BUILD #################
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -19,4 +19,33 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "EVO VPC"
   }
+}
+
+resource "aws_iam_role" "k8s" {
+  name = "EVO-k8s"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "k8s-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.k8s.name
+}
+
+resource "aws_iam_role_policy_attachment" "k8s-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.k8s.name
 }
