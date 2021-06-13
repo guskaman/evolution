318d8fffb62f1f6cb3c9d786f9108eba69675452
guseman <guseman@gmail.com>
2021-06-13 20:49:20 +0300

    acces key variables

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "eu-west-1"
+  access_key = "$AWS_ACCESS_KEY_ID"
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
