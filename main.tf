8c0dc560057670154589afef43490709a1f048d4
guseman <guseman@gmail.com>
2021-06-13 23:12:28 +0300

    network stuff

--- main.tf
+++ main.tf
@@ -12,15 +12,53 @@ terraform {
 
 ################# BUILD #################
 
+
+#network
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "192.168.88.0/24"
   instance_tenancy = "default"
 
   tags = {
-    Name = "EVO VPC"
+    Name = "EVO"
   }
 }
 
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.vpc_id
+  cidr_block = "192.168.88.0/26"
+
+  tags = {
+    Name = "K8S"
+  }
+}
+
+resource "aws_vpc" "allow_all" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+#permissions
 resource "aws_iam_role" "k8s" {
   name = "EVO-k8s"
 
@@ -49,3 +87,5 @@ resource "aws_iam_role_policy_attachment" "k8s-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.k8s.name
 }
+
+#cluster
