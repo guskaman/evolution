1e18b5853f6b119eae1983858e463683a31711cb
guseman <guseman@gmail.com>
2021-06-13 21:54:05 +0300

    tf state added

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ provider "aws" {
   # secret_key = "$AWS_SECRET_ACCESS_KEY"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "evo-tf-state"
+    key    = "statefile/"
+    region = "eu-west-1"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
